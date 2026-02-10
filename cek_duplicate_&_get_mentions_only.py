1339db6c2dd77adb3af9ae5c463d03f79e50b4f9
najmazlm <165017314+najmazlm@users.noreply.github.com>
2026-02-10 20:09:28 +0700

    Update cek_duplicate_&_get_mentions_only.py

--- cek_duplicate_&_get_mentions_only.py
+++ cek_duplicate_&_get_mentions_only.py
@@ -3,20 +3,15 @@ import pandas as pd
 
 # Misalkan Anda telah memuat DataFrame Anda, atau Anda dapat menggantinya dengan cara yang sesuai
 df = pd.read_excel("theerastour1.xlsx")
-df
 
 # Drop duplicates based on 'text_column'
 df_no_duplicates = df.drop_duplicates(subset='full_text')
-
-# Display the result
-df_no_duplicates
 
 # Mencari baris yang berisi mention (@)
 mention_condition = df_no_duplicates['full_text'].str.contains('@', case=False)  # case=False untuk memperlakukan huruf besar dan kecil sama
 
 # Membuat DataFrame baru hanya dengan baris yang memenuhi kondisi mention
 df_with_mention = df_no_duplicates[mention_condition]
-df_with_mention
 
 def onlyMention(text):
   words = text.split()
@@ -25,10 +20,6 @@ def onlyMention(text):
 
 #df = pd.read_excel('eras only mention.xlsx')
 df_with_mention['full_text'] = df_with_mention['full_text'].apply(onlyMention)
-df_with_mention
-#df_with_mention.to_excel('eras mention.xlsx')
-
-import pandas as pd
 
 # Assuming df is your DataFrame and 'full_text' is the column containing text
 df = pd.read_excel("eras mention3.xlsx")
@@ -46,14 +37,9 @@ new_df = pd.DataFrame(split_words.tolist(), index=split_words.index).stack().res
 # Concatenate the original DataFrame with the new DataFrame
 df_split = pd.concat([df.drop('full_text', axis=1), new_df], axis=1)
 
-
-# Display the result
-df_split
-#df_split.to_excel('mention theeras.xlsx')
-
 df = pd.read_excel("mention theeras clean.xlsx")
 
 # Remove the @ symbol from the 'text_column'
 df['mention'] = df['mention'].replace('@', '', regex=True)
-df
+
 df.to_excel('mention theeras clean2.xlsx')
